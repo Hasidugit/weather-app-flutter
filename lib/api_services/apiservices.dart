f44111f6d2227917a65046135ec76dd950b05ac1
Hasidugit <144755470+Hasidugit@users.noreply.github.com>
2024-06-19 10:10:17 +0530

    Update apiservices.dart
    change api key

--- lib/api_services/apiservices.dart
+++ lib/api_services/apiservices.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter_application_weather_app/model/wethermodel.dart';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 
 class ApiService {
-  static const String apiKey = 'f5545d94473345fdb4c05855241805';
+  static const String apiKey = '757ac092759d44bfab514028241906';
 
   static Future<Weather> fetchWeather(String query) async {
     final apiUrl =
-        'https://api.weatherapi.com/v1/marine.json?key=$apiKey&q=$query&days=1';
+        'http://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$query&days=1&aqi=yes&alerts=no';
 
     final response = await http.get(Uri.parse(apiUrl));
 
@@ -16,7 +17,7 @@ class ApiService {
       // If the server returns a 200 OK response, parse the JSON
       return Weather.fromJson(json.decode(response.body));
     } else {
-      // If the server did not return a 200 OK response, throw an exception
+      Logger().e(response);
       throw Exception('Failed to load weather data');
     }
   }
